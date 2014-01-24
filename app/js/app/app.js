2d5f187bb19051589b711c759e6b07d075af0f97
ciech <marcin.ciechowicz@gmail.com>
2014-01-24 18:47:58 +0100

    wr

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -46,21 +46,39 @@ visaaApp.factory('configService', function()
 
 function FieldController($scope, configService, fieldName){
  
-    $scope.value = "";
- 
-    // Mixin scope methods.
-    $scope.set = function(){
+    $scope.set = function(value){
+    	console.log("Scope value = " + value);
         configService.currentField = fieldName;
-        configService.data[fieldName][configService.currentField] = $scope.value;
-        console.log("for " +  configService.currentField + " result " + $scope.value ) 
+        configService.data[fieldName].value = value;
+        $scope.validate();
+
     };
+
+    $scope.unSet = function(){
+    	if (configService.data[fieldName].isSet === true)
+    	{
+    		configService.data[fieldName].isSet = false;
+      	}
+
+    }
+
 
     $scope.isSet = function(){
         return configService.data[fieldName].isSet;
     };
+
+    $scope.validate = function(){
+    	if (fieldName === "algorithm" || fieldName === "workmode"){
+    		configService.data[fieldName].isSet = true;
+    	}
+
+    }
 }
 
 function AdditionalParamsController($scope, configService){
+
+	$scope.html = "";
+
 	$scope.getCurrentFieldName = function(){
 		return configService.currentField;
 	}
@@ -73,8 +91,42 @@ function AdditionalParamsController($scope, configService){
 
 	}
 
+	$scope.is = function(name, value){
+		if ($scope.getCurrentFieldName() === name && 
+			$scope.getCurrentFieldValue() === value)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	$scope.showButton = function(){
+		if (configService.currentField != ""){
+			if (configService.currentField !== "algorithm" &&
+				configService.currentField !== "workmode")
+			{
+		return !configService.data[configService.currentField].isSet;
+	}
+	}
+	}
+
+	$scope.generateInput = function(){
+		$scope.seq1 = "AGCACACA";
+		$scope.seq2 = "ACACACTA";
+	}
+
+		$scope.generateLinearGap = function(){
+		$scope.linear = "-2";
+	}
+
+		$scope.generateAffineGap = function(){
+		$scope.affine_open = "-3";
+		$scope.affine_extend = "-1";
+	}
+
 	$scope.doneCurrentField = function(){
 		configService.data[configService.currentField].isSet = true;
+		configService.currentField = "";
 	}
 }
 
